93baf626ed207df3968498670dfc807ca5af12a1
Trần Nguyễn Thành Hiển <thanhhien.work.2004@gmail.com>
2025-11-23 09:34:59 +0700

    fi url

--- src/libs/apis/admin-and-staff.ts
+++ src/libs/apis/admin-and-staff.ts
@@ -2,7 +2,7 @@ import { Vehicle } from "@/types/vehicles.type";
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+  baseURL: process.env.NEXT_PUBLIC_API_ADMIN || 'http://localhost:8085/admin',
 });
 
 export default api;
--- src/libs/apis/user.ts
+++ src/libs/apis/user.ts
@@ -1,11 +1,10 @@
 // src/libs/apis/user.ts
 
 import { User } from "@/types/user";
-
-const API_BASE = 'http://localhost:8085';
+const API_USER = process.env.NEXT_PUBLIC_API_USER || 'http://localhost:8085/user';
 
 export async function fetchUsers(): Promise<User[]> {
-  const res = await fetch(`${API_BASE}/user/users/get`, {
+  const res = await fetch(`${API_USER}/users/get`, {
     method: 'GET',
     cache: 'no-store',
     credentials: 'include',
@@ -26,7 +25,7 @@ export async function fetchUsers(): Promise<User[]> {
 }
 
 export const deleteUser = async (id: number): Promise<any> => {
-  const res = await fetch(`${API_BASE}/user/users/${id}/delete_user`, {
+  const res = await fetch(`${API_USER}/users/${id}/delete_user`, {
     method: 'DELETE',
     credentials: 'include',
     headers: {
